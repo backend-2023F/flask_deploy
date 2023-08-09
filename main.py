ebb2bec68100a3d2514c8201005cac9a37ebf569
JavohirJalilov <javohirjaliloff@gmail.com>
2023-08-09 17:55:05 +0500

    setwebhook

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, request
 import requests
+import telegram
 
 TOKEN = "5959656588:AAFRUBI1vozVI4ScI9HCVu3YRk2qd_6PjxQ"
 app = Flask(__name__)
@@ -7,6 +8,13 @@ app = Flask(__name__)
 @app.route("/", methods=["GET"])
 def index():
     return f"Bot: {TOKEN}"
+
+@app.route("/set", methods=["GET"])
+def set_webhook():
+    url = "https://backend2023f.pythonanywhere.com/webhook"
+    bot = telegram.Bot(token=TOKEN)
+    bot.delete_webhook()
+    return bot.set_webhook(url)
 
 @app.route("/webhook", methods=["POST", "GET"])
 def home():
